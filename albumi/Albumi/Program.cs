00a6c56a2b4cb81e61057ce7fb5b25b301d30066
viiru6 <lyytikainen.leo@gmail.com>
2022-01-17 12:43:36 +0200

    Albumi

---
+++ albumi/Albumi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Albumi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
