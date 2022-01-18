70a3d5b1d8cf9ea6815c9d001a9fffa75c2154b3
viiru6 <lyytikainen.leo@gmail.com>
2022-01-18 13:52:35 +0200

    laskin valmis

---
+++ laskin/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace laskin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )
+            {
+                string b = "";
+                Console.WriteLine("Toiminnot: <summa> <erotus> <kertolasku> <jakolasku>");
+                b = Console.ReadLine();
+
+                if (b.ToLower() == "summa")
+                {
+                    laskin.Summa();
+                }
+                if (b.ToLower() == "erotus")
+                {
+                    laskin.Erotus();
+                }
+                if (b.ToLower() == "kertolasku")
+                {
+                    laskin.Kertolasku();
+                }
+                if (b.ToLower() == "jakolasku")
+                {
+                    laskin.Jako();
+                }
+            }
+
+        }
+    }
+}
---
+++ laskin/laskin.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace laskin
+{
+    class laskin
+    {
+        public static int a;
+        public static int b;
+        public static void numerot()
+        {
+            
+            Console.WriteLine("anna numero jota haluat käsitellä:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("anna numero jolla haluat käsitellä:");
+            b = Convert.ToInt32(Console.ReadLine());
+        }
+        public static void Summa()
+        {
+            numerot();
+            Console.Write("numeroiden ");
+            Console.Write(a + " ");
+            Console.Write("ja " + b);
+            Console.Write(" summa on: ");
+            Console.WriteLine(a + b + "\n");
+        }
+        public static void Erotus()
+        {
+            numerot();
+            Console.Write("numeroiden ");
+            Console.Write(a + " ");
+            Console.Write("ja " + b);
+            Console.Write(" erotus on: ");
+            Console.WriteLine(a - b + "\n");
+        }
+        public static void Kertolasku()
+        {
+            numerot();
+            Console.Write("numero ");
+            Console.Write(a + " kerrottuna numerolla ");
+            Console.Write(b);
+            Console.Write(" on: ");
+            Console.WriteLine(a * b + "\n");
+        }
+        public static void Jako()
+        {
+            numerot();
+            Console.Write("numero ");
+            Console.Write(a + " jaettuna numerolla ");
+            Console.Write(b);
+            Console.Write(" on: ");
+            Console.WriteLine(a / b + "\n");
+        }
+
+    }
+}
