cef643b5365b895302945228a0973894711cb62e
viiru6 <lyytikainen.leo@gmail.com>
2022-01-20 21:36:08 +0200

    1

---
+++ harjoitus 7 hissi/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace harjoitus_7_hissi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            hissi hissi = new hissi();
+            hissi.kerroksenKysyminen();
+
+
+
+
+        }
+
+    }
+}
---
+++ harjoitus 7 hissi/hissi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace harjoitus_7_hissi
+{
+    class hissi
+    {
+        static int kerros = 0;
+        private readonly int minKerros = 0;
+        private readonly int maxKerros = 6;
+        public static void kerroksenKysyminen()
+        {
+            Console.WriteLine("mihin kerrokseen haluat 1-6?");
+            try
+            {
+                kerros = Convert.ToInt32(Console.ReadLine());
+            }
+             catch{
+                Console.WriteLine("virheellinen syöte");
+            }
+
+
+        }
+
+
+    }
+}
