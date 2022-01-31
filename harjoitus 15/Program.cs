09dde3cbc299038137d1bb1e327b7a16b8f493d9
viiru6 <lyytikainen.leo@gmail.com>
2022-01-31 12:26:07 +0200

    harjoitus 15 valmis

---
+++ harjoitus 15/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace harjoitus_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+        }
+    }
+}
---
+++ harjoitus 155/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace harjoitus_155
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        static int henkilöA = 0;
+        static int avoA = 0;
+        private void onClick_henkilö(object sender, RoutedEventArgs e)
+        {
+            henkilöA++;
+            henkilöAutoLaskuri.Text = henkilöA.ToString();
+        }
+
+        private void onClickAvo(object sender, RoutedEventArgs e)
+        {
+            avoA++;
+            AvoAutoLaskuri.Text = avoA.ToString();
+        }
+    }
+}
