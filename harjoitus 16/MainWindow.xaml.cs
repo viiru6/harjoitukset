dbdfbad077b96535fca0df55f6f0d6b857e9e6e4
viiru6 <lyytikainen.leo@gmail.com>
2022-02-02 12:25:25 +0200

    harjoitus 17 valmis

---
+++ harjoitus 16/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace harjoitus_16
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+        int i = 0;
+        private void onClick(object sender, RoutedEventArgs e)
+        {
+            i++;
+            try
+            {
+                virheilmoitukset.Text = "";
+                kilometrit.Text = "0";
+                mailit.Text = "0";
+                if (i % 2 != 0)
+                {
+                    double km;
+                    km = Convert.ToDouble(kilometrit);
+                    double kmToM = km * 0.621371D;
+                    kilometrit.Text = km.ToString();
+                    mailit.Text = kmToM.ToString();
+                }
+                else
+                {
+                    string sm = mailit.ToString();
+                    double m = Convert.ToDouble(sm);
+                    double mToKm = m * 1.609344D;
+                    mailit.Text =  m.ToString();
+                    kilometrit.Text = mToKm.ToString();
+                }
+            }
+            catch(Exception)
+            {
+                virheilmoitukset.Text = "virhe";
+                Console.Beep();
+                Console.Beep();
+            }
+        }
+    }
+}
---
+++ harjoitus 17/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace harjoitus_17
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        private void keittiönValotONpäällä(object sender, RoutedEventArgs e)
+        {
+            KeittiönValotStatus.Foreground = Brushes.Green;
+            KeittiönValotStatus.Text = "Keittiön valot ovat päällä";
+        }
+        private void keittiönValotEIpäällä(object sender, RoutedEventArgs e)
+        {
+            KeittiönValotStatus.Foreground = Brushes.Red;
+            KeittiönValotStatus.Text = "Keittiön valot eivät ole päällä";
+        }
+
+        private void OloHuonenValotEIpäällä(object sender, RoutedEventArgs e)
+        {
+            OlohuoneenValotStatus.Foreground = Brushes.Red;
+            OlohuoneenValotStatus.Text = "olohuoneen valot eivät ole päällä";
+        }
+
+        private void OloHuoneenValotONpäällä(object sender, RoutedEventArgs e)
+        {
+            OlohuoneenValotStatus.Foreground = Brushes.Green;
+            OlohuoneenValotStatus.Text = "olohuoneen valot ovat päällä";
+        }
+
+        private void OviONLukossa(object sender, RoutedEventArgs e)
+        {
+            oviLukossaStatus.Foreground = Brushes.Green;
+            oviLukossaStatus.Text = "ovi on lukossa";
+        }
+
+        private void OviEIoleLukossa(object sender, RoutedEventArgs e)
+        {
+            oviLukossaStatus.Foreground = Brushes.Red;
+            oviLukossaStatus.Text = "ovi ei ole lukossa";
+        }
+        static double läm = 20D;
+        private void lämpötilaNostoNappiOnClick(object sender, RoutedEventArgs e)
+        {
+
+            läm = läm + 0.5D;
+            lämpötila.Text = läm.ToString();
+        }
+        private void lämpötilaVähennysNappiOnClick(object sender, RoutedEventArgs e)
+        {
+
+            läm = läm - 0.5D;
+            lämpötila.Text = läm.ToString();
+        }
+    }
+}
