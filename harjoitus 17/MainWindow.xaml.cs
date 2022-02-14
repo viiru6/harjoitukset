26f3e7ddfe885cd999470d4263441d128ea9e81b
viiru6 <lyytikainen.leo@gmail.com>
2022-02-14 12:42:31 +0200

    harjoitus 17 kommentit

--- harjoitus 17/MainWindow.xaml.cs
+++ harjoitus 17/MainWindow.xaml.cs
@@ -25,48 +25,44 @@ namespace harjoitus_17
             InitializeComponent();
             
         }
-        private void keittiönValotONpäällä(object sender, RoutedEventArgs e)
+        private void keittiönValotONpäällä(object sender, RoutedEventArgs e)//jos valojen ruutu on täytetty
         {
-            KeittiönValotStatus.Foreground = Brushes.Green;
+            KeittiönValotStatus.Foreground = Brushes.Green;//muuttaa keittiön valojen tekstin vihreäksi 
             KeittiönValotStatus.Text = "Keittiön valot ovat päällä";
         }
-        private void keittiönValotEIpäällä(object sender, RoutedEventArgs e)
+        private void keittiönValotEIpäällä(object sender, RoutedEventArgs e)//jos valojen ruutu ei ole täytetty
         {
-            KeittiönValotStatus.Foreground = Brushes.Red;
+            KeittiönValotStatus.Foreground = Brushes.Red;//muuttaa keittiön valojen tekstin punaiseksi 
             KeittiönValotStatus.Text = "Keittiön valot eivät ole päällä";
         }
-
-        private void OloHuonenValotEIpäällä(object sender, RoutedEventArgs e)
+        private void OloHuonenValotEIpäällä(object sender, RoutedEventArgs e)//jos olohuoneen ruutu ei ole täytetty
         {
-            OlohuoneenValotStatus.Foreground = Brushes.Red;
+            OlohuoneenValotStatus.Foreground = Brushes.Red;//muuttaa olohuoneen valojen tekstin punaiseksi 
             OlohuoneenValotStatus.Text = "olohuoneen valot eivät ole päällä";
         }
-
-        private void OloHuoneenValotONpäällä(object sender, RoutedEventArgs e)
+        private void OloHuoneenValotONpäällä(object sender, RoutedEventArgs e)//jos olohuoneen ruutu on täytetty
         {
-            OlohuoneenValotStatus.Foreground = Brushes.Green;
+            OlohuoneenValotStatus.Foreground = Brushes.Green;//muuttaa olohuoneen valojen tekstin vihreäksi 
             OlohuoneenValotStatus.Text = "olohuoneen valot ovat päällä";
         }
-
-        private void OviONLukossa(object sender, RoutedEventArgs e)
+        private void OviONLukossa(object sender, RoutedEventArgs e)//jos oven ruutu on täytetty
         {
-            oviLukossaStatus.Foreground = Brushes.Green;
+            oviLukossaStatus.Foreground = Brushes.Green;//muuttaa oven tekstin vihreäksi 
             oviLukossaStatus.Text = "ovi on lukossa";
         }
-
-        private void OviEIoleLukossa(object sender, RoutedEventArgs e)
+        private void OviEIoleLukossa(object sender, RoutedEventArgs e)//jos oven ruutu ei ole täytetty
         {
-            oviLukossaStatus.Foreground = Brushes.Red;
+            oviLukossaStatus.Foreground = Brushes.Red;//muuttaa oven tekstin punaiseksi 
             oviLukossaStatus.Text = "ovi ei ole lukossa";
         }
         static double läm = 20D;
-        private void lämpötilaNostoNappiOnClick(object sender, RoutedEventArgs e)
+        private void lämpötilaNostoNappiOnClick(object sender, RoutedEventArgs e)//jos nosta lämpöä nappia painetaan
         {
 
             läm = läm + 0.5D;
             lämpötila.Text = läm.ToString();
         }
-        private void lämpötilaVähennysNappiOnClick(object sender, RoutedEventArgs e)
+        private void lämpötilaVähennysNappiOnClick(object sender, RoutedEventArgs e)//jos laske lämpöä nappia painetaan
         {
 
             läm = läm - 0.5D;
