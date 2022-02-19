996955c79145592efe23e5c9df8434cb55ef3b58
viiru6 <lyytikainen.leo@gmail.com>
2022-02-19 13:46:21 +0200

    1

--- harjoitus 25/MainWindow.xaml.cs
+++ harjoitus 25/MainWindow.xaml.cs
@@ -43,132 +43,207 @@ namespace harjoitus_25
         }
         public void Button_Click(object sender, RoutedEventArgs e)
         {
-            virheText.Foreground = Brushes.Red;//asettaa virhe textille värin
+            virheText.Foreground = Brushes.Red;//asettaa virheTextille värin
             virheText.Text = "";//resetoi virhe textin
-            if(joukkueValinta == 1)//kun joukkue kissat on valittu ja käyttäjä lisää pelaajan
+            if(joukkueValinta == 1)//kun joukkue kissat on valittu ja käyttäjä lisää pelaajan annettujen tietojen mukaan
             {
-                pelaajaMäärä1 = pelaajaMäärä1 + 1; //tiimin pelaaja laskuri nousee yhdellä
-                if (b1 == 0)//tulostaa joukkueen tiedot kerran ja asettaa muuttujan niin että ne eivät tulostu uudelleen ennen tyhjennystä
+                taulu1Joukkue.Items.Remove(pelaajaMäärä1.ToString());//poistaa pelaaja määrän taulusta
+                if (b1 == 0)//tulostaa joukkueen tiedot kerran ja asettaa muuttujan niin että ne eivät
+                            //tulostu uudelleen ennen tyhjennystä eli metodiea t1()
                 {
                     taulu1Joukkue.Items.Add("joukkue:\nkissat\nkotikaupunki:\nTampere\npelaajat:");
                     b1 = 1;
                 }
-                taulu1Joukkue.Items.Remove((pelaajaMäärä1 - 1).ToString());//poistaa edellisen tulostetun pelaaja määrän
-                taulu1Joukkue.Items.Add(pelaajaMäärä1.ToString());//tulostaa uuden pelaaja määrän
                 taulu1.FontSize = 10;
                 Pelaaja.en = Convert.ToString(EtunimiText.Text);//tallentaa käyttäjän antaman etunimen muuttujaan
-                                                                //ja lisää sen tauluun
-                taulu1.Items.Add(en);
-                taulu1Joukkue.FontSize = 10;
                 Pelaaja.sn = Convert.ToString(SukunimiText.Text);//tallentaa käyttäjän antaman sukunimen muuttujaan
-                                                                 //ja lisää sen tauluun
-                taulu1.Items.Add(sn);
                 try
                 {
                     Pelaaja.i = Convert.ToDouble(IkäText.Text);//tallentaa käyttäjän antaman iän muuttujaan
-                                                               //ja lisää sen tauluun
-                    taulu1.Items.Add(i);
                     Pelaaja.p = Convert.ToInt32(PelaajaNumeroText.Text);//tallentaa käyttäjän antaman pelaaja numeron muuttujaan
-                                                                        //ja lisää sen tauluun
-                                                                        //sama koodi toistuu vielä kaksi kertaa mutta niissä tieto tallentuu eri
-                                                                        //tauluihin
-                    taulu1.Items.Add(p);
                 }
                 catch
                 {
                     virheText.Text = "virhe";
                 }
+                if (Pelaaja.en != "")//jos EtunimiText ei ole tyhjä
+                {
+                    if (Pelaaja.sn != "")//jos SukunimiText ei ole tyhjä
+                    {
+                        List<string> testilista = new List<string>();
+                        pelaajaMäärä1 = pelaajaMäärä1 + 1; //tiimin pelaaja laskuri nousee yhdellä
+                        taulu1Joukkue.Items.Add(pelaajaMäärä1.ToString());//tulostaa uuden pelaaja määrän
+                        taulu1.Items.Add(en);
+                        taulu1.Items.Add(sn);
+                        taulu1.Items.Add(i);
+                        taulu1.Items.Add(p);
+                        testilista.Add(en);
+                        testilista.Add(sn);
+                        testilista.Add(i.ToString());
+                        testilista.Add(p.ToString());
+                    }
+                    else
+                    {
+                        virheText.Text = "täytä kaikki ruudut";//ilmoittaa virheestä
+                    }
+                }
+                else
+                {
+                    virheText.Text = "täytä kaikki ruudut";//ilmoittaa virheestä
+                    taulu1Joukkue.Items.Remove(pelaajaMäärä1.ToString());//korjaa pelaaja määrä laskurin
+                    if(pelaajaMäärä1 > 0)
+                    {
+                        pelaajaMäärä1 = pelaajaMäärä1 - 1;//korjaa pelaaja määrä laskurin
+                    }
+                }
+                taulu1Joukkue.FontSize = 10;
             }
             if(joukkueValinta == 2)
             {
-                pelaajaMäärä2 = pelaajaMäärä2 + 1;
-                if (b2 == 0)
+                taulu2Joukkue.Items.Remove(pelaajaMäärä2.ToString());//poistaa pelaaja määrän taulusta
+                if (b2 == 0)//tulostaa joukkueen tiedot kerran ja asettaa muuttujan niin että ne eivät
+                            //tulostu uudelleen ennen tyhjennystä eli metodiea t1()
                 {
                     taulu2Joukkue.Items.Add("joukkue:\nkoirat\nkotikaupunki:\nTurku\npelaajat:");
                     b2 = 1;
                 }
-                taulu2Joukkue.Items.Remove((pelaajaMäärä2 - 1).ToString());
-                taulu2Joukkue.Items.Add(pelaajaMäärä2.ToString());
                 taulu2.FontSize = 10;
-                Pelaaja.en = Convert.ToString(EtunimiText.Text);
-                taulu2.Items.Add(en);
-                taulu2Joukkue.FontSize = 10;
-                Pelaaja.sn = Convert.ToString(SukunimiText.Text);
-                taulu2.Items.Add(sn);
+                Pelaaja.en = Convert.ToString(EtunimiText.Text);//tallentaa käyttäjän antaman etunimen muuttujaan
+                Pelaaja.sn = Convert.ToString(SukunimiText.Text);//tallentaa käyttäjän antaman sukunimen muuttujaan
                 try
                 {
-                    Pelaaja.i = Convert.ToDouble(IkäText.Text);
-                    taulu2.Items.Add(i);
-                    Pelaaja.p = Convert.ToInt32(PelaajaNumeroText.Text);
-                    taulu2.Items.Add(p);
+                    Pelaaja.i = Convert.ToDouble(IkäText.Text);//tallentaa käyttäjän antaman iän muuttujaan
+                    Pelaaja.p = Convert.ToInt32(PelaajaNumeroText.Text);//tallentaa käyttäjän antaman pelaaja numeron muuttujaan
                 }
                 catch
                 {
                     virheText.Text = "virhe";
                 }
+                if (Pelaaja.en != "")//jos EtunimiText ei ole tyhjä
+                {
+                    if (Pelaaja.sn != "")//jos SukunimiText ei ole tyhjä
+                    {
+                        pelaajaMäärä2 = pelaajaMäärä2 + 1; //tiimin pelaaja laskuri nousee yhdellä
+                        taulu2Joukkue.Items.Add(pelaajaMäärä2.ToString());//tulostaa uuden pelaaja määrän
+                        taulu2.Items.Add(en);
+                        taulu2.Items.Add(sn);
+                        taulu2.Items.Add(i);
+                        taulu2.Items.Add(p);
+                    }
+                    else
+                    {
+                        virheText.Text = "täytä kaikki ruudut";//ilmoittaa virheestä
+                    }
+                }
+                else
+                {
+                    virheText.Text = "täytä kaikki ruudut";//ilmoittaa virheestä
+                    taulu2Joukkue.Items.Remove(pelaajaMäärä2.ToString());//korjaa pelaaja määrä laskurin
+                    if (pelaajaMäärä2 > 0)
+                    {
+                        pelaajaMäärä2 = pelaajaMäärä2 - 1;//korjaa pelaaja määrä laskurin
+                    }
+                }
+                taulu2Joukkue.FontSize = 10;
             }
-            if(joukkueValinta == 3)
+            if (joukkueValinta == 3)
             {
-                pelaajaMäärä3 = pelaajaMäärä3 + 1;
-                if (b3 == 0)
+                taulu3Joukkue.Items.Remove(pelaajaMäärä3.ToString());//poistaa pelaaja määrän taulusta
+                if (b3 == 0)//tulostaa joukkueen tiedot kerran ja asettaa muuttujan niin että ne eivät
+                            //tulostu uudelleen ennen tyhjennystä eli metodiea t1()
                 {
                     taulu3Joukkue.Items.Add("joukkue:\nrotat\nkotikaupunki:\nHelsinki\npelaajat:");
                     b3 = 1;
                 }
-                taulu3Joukkue.Items.Remove((pelaajaMäärä3 - 1).ToString());
-                taulu3Joukkue.Items.Add(pelaajaMäärä3.ToString());
                 taulu3.FontSize = 10;
-                Pelaaja.en = Convert.ToString(EtunimiText.Text);
-                taulu3.Items.Add(en);
-                taulu3Joukkue.FontSize = 10;
-                Pelaaja.sn = Convert.ToString(SukunimiText.Text);
-                taulu3.Items.Add(sn);
+                Pelaaja.en = Convert.ToString(EtunimiText.Text);//tallentaa käyttäjän antaman etunimen muuttujaan
+                Pelaaja.sn = Convert.ToString(SukunimiText.Text);//tallentaa käyttäjän antaman sukunimen muuttujaan
                 try
                 {
-                    Pelaaja.i = Convert.ToDouble(IkäText.Text);
-                    taulu3.Items.Add(i);
-                    Pelaaja.p = Convert.ToInt32(PelaajaNumeroText.Text);
-                    taulu3.Items.Add(p);
+                    Pelaaja.i = Convert.ToDouble(IkäText.Text);//tallentaa käyttäjän antaman iän muuttujaan
+                    Pelaaja.p = Convert.ToInt32(PelaajaNumeroText.Text);//tallentaa käyttäjän antaman pelaaja numeron muuttujaan
                 }
                 catch
                 {
                     virheText.Text = "virhe";
                 }
+                if (Pelaaja.en != "")//jos EtunimiText ei ole tyhjä
+                {
+                    if (Pelaaja.sn != "")//jos SukunimiText ei ole tyhjä
+                    {
+                        pelaajaMäärä3 = pelaajaMäärä3 + 1; //tiimin pelaaja laskuri nousee yhdellä
+                        taulu3Joukkue.Items.Add(pelaajaMäärä3.ToString());//tulostaa uuden pelaaja määrän
+                        taulu3.Items.Add(en);
+                        taulu3.Items.Add(sn);
+                        taulu3.Items.Add(i);
+                        taulu3.Items.Add(p);
+                    }
+                    else
+                    {
+                        virheText.Text = "täytä kaikki ruudut";//ilmoittaa virheestä
+                    }
+                }
+                else
+                {
+                    virheText.Text = "täytä kaikki ruudut";//ilmoittaa virheestä
+                    taulu3Joukkue.Items.Remove(pelaajaMäärä3.ToString());//korjaa pelaaja määrä laskurin
+                    if (pelaajaMäärä3 > 0)
+                    {
+                        pelaajaMäärä3 = pelaajaMäärä3 - 1;//korjaa pelaaja määrä laskurin
+                    }
+
+                }
+                taulu3Joukkue.FontSize = 10;
             }
         }
         private void Button_Click2(object sender, RoutedEventArgs e)
         {
-            if (en == EtunimiText.Text)
+            if (en == EtunimiText.Text)//jos annettu etunimi on sama kuin etunimen textbox niin se poistetaan listasta
             {
                 if (joukkueValinta == 1)
                 {
-                    if (taulu1.Items.IsEmpty)
+                    taulu1Joukkue.Items.Remove(pelaajaMäärä1.ToString());
+                    if (taulu1.Items.IsEmpty)//jos lista on tyhjä niin kaikki nollataan
                     {
                         taulu1Joukkue.Items.Clear();
                         t1();
                     }
+                    else//jos ei ole tyhjä niin pelaaja määrä laskee yhdenllä koska pelaaja on poistettu
+                    {
+                        pelaajaMäärä1 = pelaajaMäärä1 - 1;
+                    }
                     taulu1.Items.Remove(en);
                 }
-                if (joukkueValinta == 2)
+                if (joukkueValinta == 2)//jos annettu etunimi on sama kuin etunimen textbox niin se poistetaan listasta
                 {
-                    if (taulu2.Items.IsEmpty)
+                    taulu2Joukkue.Items.Remove(pelaajaMäärä2.ToString());
+                    if (taulu2.Items.IsEmpty)//jos lista on tyhjä niin kaikki nollataan
                     {
                         taulu2Joukkue.Items.Clear();
                         t2();
                     }
+                    else//jos ei ole tyhjä niin pelaaja määrä laskee yhdenllä koska pelaaja on poistettu
+                    {
+                        pelaajaMäärä2 = pelaajaMäärä2 - 1;
+                    }
                     taulu2.Items.Remove(en);
                 }
-                if (joukkueValinta == 3)
+                if (joukkueValinta == 3)//jos annettu etunimi on sama kuin etunimen textbox niin se poistetaan listasta
                 {
-                    if (taulu3.Items.IsEmpty)
+                    taulu3Joukkue.Items.Remove(pelaajaMäärä3.ToString());
+                    if (taulu3.Items.IsEmpty)//jos lista on tyhjä niin kaikki nollataan
                     {
                         taulu3Joukkue.Items.Clear();
                         t3();
                     }
+                    else//jos ei ole tyhjä niin pelaaja määrä laskee yhdenllä koska pelaaja on poistettu
+                    {
+                        pelaajaMäärä3 = pelaajaMäärä3 - 1;
+                    }
                     taulu3.Items.Remove(en);
                 }
             }
-            if (sn == SukunimiText.Text)
+            if (sn == SukunimiText.Text)//jos annettu sukunimi on sama kuin Sukunimen textbox niin se poistetaan listasta
             {
                 if (joukkueValinta == 1)
                 {
@@ -185,7 +260,7 @@ namespace harjoitus_25
             }
             try
             {
-                if (i == Convert.ToDouble(IkäText.Text))
+                if (i == Convert.ToDouble(IkäText.Text))//jos annettu ikä on sama kuin iän textbox niin se poistetaan listasta
                 {
                     if (joukkueValinta == 1)
                     {
@@ -207,7 +282,8 @@ namespace harjoitus_25
             }
             try
             {
-                if (p == Convert.ToInt32(PelaajaNumeroText.Text))
+                if (p == Convert.ToInt32(PelaajaNumeroText.Text))//jos annettu pelaaja numero on sama kuin
+                                                                 //pelaaja numeron textbox niin se poistetaan listasta
                 {
                     if (joukkueValinta == 1)
                     {
@@ -273,13 +349,15 @@ namespace harjoitus_25
         {
             t3();
         }
-        private void TallennaNappi_Click(object sender, RoutedEventArgs e)
+        private void TallennaNappi_Click(object sender, RoutedEventArgs e)//serialisoinin kokeilu mutta en onnistunut jatkamaan ja serialisoimaan listoja
         {
             Joukkue joukkue = new Joukkue { joukkueenNimi = "hevoset" };
             Stream writestream = new FileStream("harjoitus25.bin", FileMode.Create, FileAccess.Write, FileShare.None);
             IFormatter formatter = new BinaryFormatter();
             formatter.Serialize(writestream, joukkue);
             writestream.Close();
+
+            
         }
         private void LataaNappi_Click(object sender, RoutedEventArgs e)
         {
