0b4ee67c42e5708a727bc42802e1a478beca9686
viiru6 <lyytikainen.leo@gmail.com>
2022-03-04 10:34:40 +0200

    26 kommentoitu ja valmis?

--- harjoitus 26/MainWindow.xaml.cs
+++ harjoitus 26/MainWindow.xaml.cs
@@ -25,29 +25,46 @@ namespace harjoitus_26
             InitializeComponent();
         }
         string t;
-
-        private void LisääNappi_Click(object sender, RoutedEventArgs e)
+        int Numero;
+        public void LisääNappi_Click(object sender, RoutedEventArgs e)
         {
-            CheckBox b = new CheckBox();
-            Lista2.Items.Add(b);
-            t = Convert.ToString(Teksti.Text);
-            Lista.Items.Add(t);
+            Lista.FontSize = 11.38;//korjaa fontin
+            if(Teksti.Text != "")//jos teksti kenttä ei ole tyhjä niin luo uuden checkboxin ja kirjoittaa sen tooltipim ja sitten sisää sen listaan2
+            {
+                CheckBox b = new CheckBox();
+                b.ToolTip = "omistatko kirjan?";
+                Lista2.Items.Add(b);
+            }
+            Lista.Items.Add(t);//lisää kirjoitetun tekstin listaan
+            if(Teksti.Text != "")
+            {
+                Numero = Numero + 1;//nostaa kirjalaskuria yhdellä
+            }
+            numero.Text = Numero.ToString();//tulostaa kirjojen määrän
         }
         private void PoistaNappi_OnClick(object sender, RoutedEventArgs e)
         {
-            if (t == Teksti.Text)
+            if (Lista.Items.Contains(t))//jos listassa on viimeksi kirjoitettu teksti niin listoista poistetaan viimeisimmät lisäykset ja numero laskuri laskee yhdellä
             {
-                Lista.Items.Remove(t);
-                Lista2.Items.Remove(b);
+                if (Numero > 0)
+                {
+                    Lista2.Items.RemoveAt(Lista2.Items.Count - 1);
+                    Lista.Items.RemoveAt(Lista.Items.Count - 1);
+                    Numero = Numero - 1;
+                }
+                numero.Text = Numero.ToString();//tulostaa kirja laskurin
             }
         }
-
-
-        private void TyhjennäNappi_Click(object sender, RoutedEventArgs e)
+        private void Teksti_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            t = Convert.ToString(Teksti.Text);//tallentaa kirjoitetun tekstin kirjoittaessa
+        }
+        private void PoistaNappi_DoubleClick(object sender, MouseButtonEventArgs e)//tyhjentää listat, resetoi kirja laskurin ja tulostaa laskurin
         {
             Lista.Items.Clear();
             Lista2.Items.Clear();
+            Numero = 0;
+            numero.Text = Numero.ToString();
         }
-        
     }
 }
